b7e317015840a6d8638b9ff53fe176a1a5094fa6
max <mwilkens@protonmail.ch>
2023-04-23 22:16:07 +0200

    remove macos stuff

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -15,13 +15,6 @@ set -euo pipefail
 
 exit_code=0
 
-# The following is for making sure the sed command works on MacOS, too.
-sedi=(-i)
-case "$(uname)" in
-  # For macOS, use two parameters
-  Darwin*) sedi=(-i "")
-esac
-
 # Iterate over all test directories
 for test_dir in tests/*; do
     test_dir_name=$(basename "${test_dir}")
@@ -32,7 +25,7 @@ for test_dir in tests/*; do
     bin/run.sh "${test_dir_name}" "${test_dir_path}" "${test_dir_path}"
 
     # Normalize the results file
-    sed "${sedi[@]}" -E \
+    sed -i -E \
       -e 's/Randomized with seed [0-9]+\\n//' \
       -e 's/Finished in [0-9]+\.[0-9]+ seconds\\n//' \
       -e 's/Completed [0-9]+ action\(s\).\\n//' \
